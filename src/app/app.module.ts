f5b7a79a2e82667d1dd5d62e33e768e47b84b69e
mcgill-a <alex.mcgill@outlook.com>
2023-01-23 21:20:45 +0000

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SnapComponent } from './snap/snap.component';
 
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: SnapComponent },
+];
+
 @NgModule({
   declarations: [AppComponent, SnapComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
