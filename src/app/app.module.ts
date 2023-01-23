d3c51a88f93fac4d323a8cec86f1a35f66829735
mcgill-a <alex.mcgill@outlook.com>
2023-01-23 21:29:39 +0000

    generate cards component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SnapComponent } from './snap/snap.component';
 import { CardComponent } from './card/card.component';
+import { CardsComponent } from './cards/cards.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: SnapComponent },
 ];
 
 @NgModule({
-  declarations: [AppComponent, SnapComponent, CardComponent],
+  declarations: [AppComponent, SnapComponent, CardComponent, CardsComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
