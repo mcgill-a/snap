fa46ae210e7b377b6858918eb596d344aced73cf
mcgill-a <alex.mcgill@outlook.com>
2023-01-23 21:17:25 +0000

    Generate snap component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SnapComponent } from './snap/snap.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SnapComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/snap/snap.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fido-snap',
+  templateUrl: './snap.component.html',
+  styleUrls: ['./snap.component.scss'],
+})
+export class SnapComponent {}
